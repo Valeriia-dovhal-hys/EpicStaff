2bdd791e6ecd3662e10c541e73d88c607ebd1db1
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-05-01 12:30:02 +0000

    Added edit file tool and test_dir for tmp files

--- tests/tools_tests/fixtures.py
+++ tests/tools_tests/fixtures.py
@@ -1,9 +1,16 @@
+from pathlib import Path
+from shutil import rmtree
+
 import pytest
 
-from src.tools import AppendFileTool
+from src.tools import AppendFileTool, EditFileTool
 from src.tools.file_create_tool import CreateFileTool
 from src.tools.file_count_lines import FileCountLinesTool
 from src.tools.file_line_read_tool import LineReadFileTool
+
+
+test_dir = "tests/tmp/"
+
 
 @pytest.fixture
 def create_file_tool():
@@ -23,3 +30,17 @@ def line_read_file_tool():
 @pytest.fixture
 def append_file_tool():
     yield AppendFileTool("predefined.txt")
+
+
+@pytest.fixture
+def edit_file_tool():
+    path = Path(test_dir)
+    path.mkdir(parents=True, exist_ok=True)
+    yield EditFileTool("predefined.txt")
+    rmtree(path)
+
+
+
+
+
+
