9bdb12059604ce0892251e57e6937df7328d4418
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-03 09:12:02 +0000

    Add vcr-test for file_line_read_tool and cassette

--- tests/tools_tests/file_line_read_tool_test.py
+++ tests/tools_tests/file_line_read_tool_test.py
@@ -1,11 +1,15 @@
+from crewai import Task
+from pathlib import Path
+
 import pytest
 import pytest_mock
 from pytest_mock import mocker
 from unittest.mock import patch
 
 from tests.mocks.tools_mocks import mock_file_with_content, mock_empty_file
-from tests.tools_tests.fixtures import line_read_file_tool
+from tests.tools_tests.fixtures import file_line_read_tool, file_line_read_tool_setup_test_dir
 from src.tools.file_line_read_tool import LineReadFileTool
+from tests.conftest import test_dir
 
 
 def get_text_lines(text, from_=0, to_=None):
@@ -35,7 +39,7 @@ class TestFileLineReadTool:
     id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. 
     Nam libero tempore, cum soluta nobis est eligendi optio, 
     cumque nihil impedit, quo minus id, quod maxime placeat, facere possimus, 
-    omnis voluptas assumenda est, omnis dolor repellendus. """
+    omnis voluptas assumenda est, omnis dolor repellendus."""
 
 
     @pytest.mark.parametrize("file_path, line_number", [
@@ -45,10 +49,10 @@ class TestFileLineReadTool:
         ("file.txt", 19),
 
     ])
-    def test_read_all_lines(self, mocker, line_read_file_tool, file_path, line_number):
+    def test_read_all_lines(self, mocker, file_line_read_tool, file_path, line_number):
         '''Test read all lines from position from file.'''
 
-        tool = line_read_file_tool
+        tool = file_line_read_tool
         mocked_open = mocker.patch("builtins.open", mock_file_with_content(self.test_text))
 
         expected = LineReadFileTool.format_lines(get_text_lines(text=self.test_text, from_=line_number-1), line_number)
@@ -58,12 +62,12 @@ class TestFileLineReadTool:
 
         assert result == expected
 
-    def test_read_all_lines_out_of_max_range(self, mocker, line_read_file_tool):
+    def test_read_all_lines_out_of_max_range(self, mocker, file_line_read_tool):
         """Test read all lines out of max range."""
 
-        line_number_out_of_max_range = len(get_text_lines(text=self.test_text))+1
+        line_number_out_of_max_range = len(get_text_lines(text=self.test_text)) + 1
 
-        tool = line_read_file_tool
+        tool = file_line_read_tool
         mocked_open = mocker.patch("builtins.open", mock_file_with_content(self.test_text))
 
         result = tool._run(line_number=line_number_out_of_max_range, number_of_lines=None)
@@ -77,10 +81,10 @@ class TestFileLineReadTool:
         -50,
         -100
     ])
-    def test_read_all_lines_out_of_min_range(self, mocker, line_read_file_tool, line_number):
+    def test_read_all_lines_out_of_min_range(self, mocker, file_line_read_tool, line_number):
         """Test read all lines out of min range."""
 
-        tool = line_read_file_tool
+        tool = file_line_read_tool
 
         result = tool._run(line_number=line_number, number_of_lines=None)
 
@@ -93,10 +97,10 @@ class TestFileLineReadTool:
         ("file.txt", 19, 1),
 
     ])
-    def test_read_n_lines(self, mocker, line_read_file_tool, file_path, line_number, num_lines):
+    def test_read_n_lines(self, mocker, file_line_read_tool, file_path, line_number, num_lines):
         '''Test read number of lines from line_number in file.'''
 
-        tool = line_read_file_tool
+        tool = file_line_read_tool
         mocked_open = mocker.patch("builtins.open", mock_file_with_content(self.test_text))
 
         expected = LineReadFileTool.format_lines(
@@ -114,13 +118,39 @@ class TestFileLineReadTool:
         -2,
         -11,
     ])
-    def test_read_negative_n_lines(self, mocker, line_read_file_tool, num_lines):
+    def test_read_negative_n_lines(self, mocker, file_line_read_tool, num_lines):
         '''Test read negative number of lines in file.'''
 
-        tool = line_read_file_tool
+        tool = file_line_read_tool
 
         expected = "I made a mistake, I forgot that number of lines has to be positive."
         result = tool._run(num_lines=num_lines)
 
         assert result == expected
+
+    @pytest.mark.vcr(filter_headers=["authorization"], record_mode="once")
+    def test_line_read_tool_with_crewai(self, agent, file_line_read_tool_setup_test_dir):
+
+        filename = "dummy.txt"
+        path = Path(test_dir)
+        filepath = path / filename
+        filepath.write_text(self.test_text)
+        abs_path = filepath.resolve()
+        
+        line_number = 17
+        expected = LineReadFileTool.format_lines(get_text_lines(text=self.test_text, from_=line_number-1), line_number)
+
+        agent.tools.append(file_line_read_tool_setup_test_dir)
+        task = Task(
+            description=f"""Read all lines starting with {line_number} from {abs_path}""",
+            agent=agent,
+            expected_output=f"""The response in the 
+            following format using relative path:
+            "I read from the file {filename} in {filepath} and here are the lines I found: \n {expected}" (without "")
+            if file is succesfully read, "Error." if not""",
+        )
+
+        output = agent.execute_task(task)
+
+        assert output == f"I read from the file {filename} in {filepath} and here are the lines I found: \n {expected}"
 
