c4427edbda55cd0c94e47df0b0104f187053ab80
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-05-05 00:00:04 +0000

    Add cassettes instead of saving csv, added exceptions

--- tests/import_tests/crewai_import_test.py
+++ tests/import_tests/crewai_import_test.py
@@ -1,4 +1,3 @@
-from typing import List
 import pytest
 import pytest_mock
 import pandas as pd
@@ -13,31 +12,17 @@ class TestImport:
     dataframe_names = ("agents", "tasks", "crew", "models", "tools")
     sheet_url = "https://docs.google.com/spreadsheets/d/1HXOAB7SLhnO9gYWFQj4l-vna9_80QoeJIp_kCEouFY0/edit?gid=868167866#gid=868167866"
 
-    # def test_create_csv_from_gsheet(
-    #     self,
-    #     mocker,
-    # ):
-    #     dataframes: List[pd.DataFrame] = Sheets.read_google_sheet(
-    #         "https://docs.google.com/spreadsheets/d/1HXOAB7SLhnO9gYWFQj4l-vna9_80QoeJIp_kCEouFY0/edit?gid=868167866#gid=868167866"
-    #     )
-    #     for name, dataframe in zip(self.dataframe_names, dataframes):
-    #         dataframe.to_csv(f"{self.resources}{name}.csv", index=False)
-
-    def load_dataframes(self):
-        return [
-            pd.read_csv(f"{self.resources}{name}.csv") for name in self.dataframe_names
-        ]
-
-    def test_import_tools(self, mocker: MockerFixture):
-
-        loaded_df = self.load_dataframes()
-
-        mocker.patch(
-            "utils.sheets_loader.Sheets.read_google_sheet", return_value=loaded_df
-        )
+    @pytest.mark.vcr(filter_headers=["authorization"], record_mode="once")
+    def test_import_tools(self):
 
         agents_df, tasks_df, crew_df, models_df, tools_df = Sheets.parse_table(
             self.sheet_url
         )
+        tools_mapping = ToolsMapping(
+            tools_df, models_df
+        )  # Get the ToolsMapping instance
+        tools_dict = (
+            tools_mapping.get_tools()
+        )  # Get the dictionary of tools from the instance
+        
 
-        print(tools_df)
