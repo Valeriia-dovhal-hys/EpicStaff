8e3559df5fbe28acaa379c190a37a0b0a6d65607
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-05 09:17:42 +0000

    Add counstruct_savepath to RouteTool
    - It contructs savepath from a given filepath, i.e. adds whole filepath
      structure as a suffix to save_file_path determined my .env file

--- src/tools/route_tool.py
+++ src/tools/route_tool.py
@@ -5,13 +5,11 @@ from typing import Optional, Any
 from crewai_tools import BaseTool
 
 
-SAVE_FILE_PATH = os.getenv("SAVE_FILE_PATH")
-
-
 class RouteTool(BaseTool):
     name: str = "Parent tool for tools that operate with paths"
     description: str = f"""Does nothing by itself, exists only to provide 
     base functionality to its child"""
+    save_file_path: Optional[str] = None
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -20,15 +18,17 @@ class RouteTool(BaseTool):
         pass
 
     @staticmethod
-    def _is_path_within_path(source_path: Path, dest_path: Path):
+    def _is_path_within_path(source_path: Path, dest_path: Path) -> bool:
         source_path = source_path.resolve()
         dest_path = dest_path.resolve()
 
         return dest_path in source_path.parents or source_path == dest_path
     
-    # TODO: it won't always work as it should for paths with /../../../ and so on, should
-    # consider this case for safety reasons as well
     @staticmethod
-    def is_path_has_permission(path: Path | str):
-        path = Path(path)
-        return RouteTool._is_path_within_path(path, Path(SAVE_FILE_PATH))
+    def is_path_has_permission(path: Path | str) -> bool:
+        save_file_path = os.getenv("SAVE_FILE_PATH")
+        return RouteTool._is_path_within_path(path, Path(save_file_path))
+    
+    def construct_savepath(self, *, frompath: Path | str) -> Path:
+        save_file_path = os.getenv("SAVE_FILE_PATH")
+        return Path(save_file_path) / Path(frompath)
