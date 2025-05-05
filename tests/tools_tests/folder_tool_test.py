3d10bbe2f984e6961a9fee1013f265e641e2c9e1
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-05 12:34:00 +0000

    Update existing tests for folder tool changes

--- tests/tools_tests/folder_tool_test.py
+++ tests/tools_tests/folder_tool_test.py
@@ -1,14 +1,17 @@
 from datetime import time
 from pathlib import Path
 from typing import Set
+from os import walk
+from crewai import Task
 
 import pytest
 import pytest_mock
 from pytest_mock import mocker
 from unittest.mock import patch, create_autospec
-from os import walk
+
 from tests.mocks.tools_mocks import mock_empty_file
-from tests.tools_tests.fixtures import folder_tool, test_dir
+from tests.tools_tests.fixtures import folder_tool
+from tests.conftest import test_dir
 
 
 def create_test_files(dirs_paths: Set[Path], file_paths: Set[Path]) -> None:
@@ -34,7 +37,7 @@ class TestFileCreateTool:
              'in.exe', 'but.ini', 'cant.py',
     ]
 
-    def test_folder_tool_recursive_true(self, mocker, folder_tool):
+    def test_folder_tool_recursive_true(self, mocker, monkeypatch, folder_tool):
         dirs_paths = {Path(test_dir) / d for d in self.dirs}
         file_paths = {Path(test_dir) / f for f in self.recursive_files + self.non_recursive_files}
 
@@ -42,18 +45,19 @@ class TestFileCreateTool:
 
         tool = folder_tool
 
-        mocked_datetime = mocker.patch('time.strftime', return_value='TEST')
+        mocked_datetime = mocker.patch('time.strftime', return_value='TEST1')
+        monkeypatch.setenv("SAVE_FILE_PATH", test_dir)
 
-        result = tool._run(folder_path=test_dir, save_directory=test_dir, recursive=True)
+        result = tool._run(folder_path=test_dir, recursive=True)
 
         expected = file_paths
-        actual = read_test_filepaths(test_dir + "find_TEST.txt")
+        actual = read_test_filepaths(test_dir + "folder_tool_outputTEST1.txt")
 
         assert actual == expected
 
         assert result.startswith(f"{len(expected)} files were listed. Here are the first 5 lines:\n")
 
-    def test_folder_tool_recursive_false(self, mocker, folder_tool):
+    def test_folder_tool_recursive_false(self, mocker, monkeypatch, folder_tool):
         dirs_paths = {Path(test_dir) / d for d in self.dirs}
         file_paths = {Path(test_dir) / f for f in self.non_recursive_files + self.recursive_files}
 
@@ -62,11 +66,28 @@ class TestFileCreateTool:
         tool = folder_tool
 
         mocker.patch('time.strftime', return_value='TEST2')
+        monkeypatch.setenv("SAVE_FILE_PATH", test_dir)
 
-        result: str = tool._run(folder_path=test_dir, save_directory=test_dir, recursive=False)
+        result: str = tool._run(folder_path=test_dir, recursive=False)
 
         expected = {Path(test_dir) / f for f in self.non_recursive_files}
-        actual = read_test_filepaths(test_dir+"find_TEST2.txt")
+        actual = read_test_filepaths(test_dir + "folder_tool_outputTEST2.txt")
         assert actual == expected
 
         assert result.startswith(f"{len(expected)} files were listed. Here are the files:\n")
+
+    @pytest.mark.skip
+    @pytest.mark.vcr(filter_headers=["authorization"], record_mode="once")
+    def test_folder_tool_with_crewai(self, agent, folder_tool):
+        
+        agent.append(folder_tool)
+        task = Task(
+            description=f"""""",
+            agent=agent,
+            epected_output=f""""""
+        )
+
+        output = agent.execute_task(task)
+
+        assert output == f""
+
