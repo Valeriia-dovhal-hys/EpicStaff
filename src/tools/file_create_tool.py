e621b19472b908ddc88f4d090424f00f2518e98f
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-05 09:16:07 +0000

    Make create file tool inherit from route tool
    - Inheritance from RouteTool, to access staticfunctions with checkers
    - Change schema for args and the logic of _run

--- src/tools/file_create_tool.py
+++ src/tools/file_create_tool.py
@@ -1,3 +1,4 @@
+import os
 import logging
 from typing import Optional, Type, Any
 
@@ -6,7 +7,7 @@ logger = logging.getLogger(__name__)
 from pydantic.v1 import BaseModel, Field
 from crewai_tools import BaseTool
 
-import os
+from src.tools.route_tool import RouteTool
 
 SAVE_FILE_PATH = os.getenv("SAVE_FILE_PATH")
 
@@ -14,11 +15,13 @@ SAVE_FILE_PATH = os.getenv("SAVE_FILE_PATH")
 class CreateFileSchema(BaseModel):
     """Input for CreateFileTool."""
 
+    file_name: str = Field(..., description="Mandatory field with the name of the file to create")
     file_path: Optional[str] = Field(..., description=f"""The relative path where the file 
-                                     should be created within the {SAVE_FILE_PATH} directory""")
+                                     should be created within the {SAVE_FILE_PATH} directory, 
+                                     excluding the file name itself""")
 
 
-class CreateFileTool(BaseTool):
+class CreateFileTool(RouteTool):
     name: str = "Create a file"
     description: str = f"""A tool that's used to create a file in 
     a directory {SAVE_FILE_PATH} combined with a user-provided file path if it's given.
@@ -26,9 +29,10 @@ class CreateFileTool(BaseTool):
     args_schema: Type[BaseModel] = CreateFileSchema
     file_path: Optional[str] = None
 
-    def __init__(self, file_path: Optional[str] = None, **kwargs):
+    def __init__(self, file_name, file_path: Optional[str] = None, **kwargs):
         super().__init__(**kwargs)
 
+        self.file_name = file_name
         self.file_path = file_path
         self._generate_description()
 
@@ -38,6 +42,11 @@ class CreateFileTool(BaseTool):
     ) -> Any:
         try:
             file_path = kwargs.get("file_path", self.file_path)
+
+            if file_path is None:
+                return "No filepath provided."
+            elif not CreateFileTool.is_path_has_permission(file_path):
+                return "Given filepath doesn't have access to the specified directory."
             with open(file_path, "x") as file:
                 return "File created successfully"
         except FileExistsError:
