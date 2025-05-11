0587fbadad7d11a89f68f987ab681d61e49794b6
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-11 16:22:56 +0000

    fix: add network and name to created container

--- tools_registry/image_files/services/runner_service.py
+++ tools_registry/image_files/services/runner_service.py
@@ -1,12 +1,20 @@
 from docker.models.images import Image
-from docker import client
+import docker
 
+client = docker.client.from_env()
 
-class RunnerSerivce:
+class RunnerService:
 
     def __init__(self): ...
 
-    def run_image(image: Image, port: int = 0):
+    def run_image(self, image: Image, tool_alias=None, port: int = 0):
         container_tool = client.containers.run(
-            image=image, ports={"8000/tcp": port}, detach=True
+            image=image, 
+            ports={"8000/tcp": port}, 
+            network="my-net",
+            detach=True,
+            name=tool_alias
         )
+
+        return container_tool
+    
