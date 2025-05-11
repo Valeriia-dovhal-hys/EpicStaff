c963d43cc6f809b2a1ddced924f04e0e312e667f
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-11 12:30:39 +0000

    feat(get_class_data): add tool's create and call logic
    Note: not working yet

--- tools_registry/image_files/app.py
+++ tools_registry/image_files/app.py
@@ -3,9 +3,19 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 import uvicorn
+import requests
 
 from models.models import RunToolModel
-from tools_registry.image_files.repositories.import_tool_data_repository import ImportToolDataRepository
+from repositories.import_tool_data_repository import ImportToolDataRepository
+from services.tool_image_builder_service import ToolImageBuilderService
+from services.runner_service import RunnerService
+from services.registry import Registry
+
+import docker
+from docker.models.containers import Container
+
+docker.from_env()
+client: Container = docker.client
 
 app = FastAPI()
 itdb = ImportToolDataRepository()
@@ -13,15 +23,19 @@ itdb = ImportToolDataRepository()
 
 @app.get("/tool/list", status_code=200)
 async def get_all_classes_data(tool_alias: str):
-
     return
-    # TODO: Then we shall run container with tool
 
 
 @app.get("/tool/{tool_alias}/class-data", status_code=200)
 async def get_class_data(tool_alias: str):
+    ibs = ToolImageBuilderService(Registry(), itdb)
+    image = ibs.build_tool_alias(tool_alias)
+
+    container = RunnerService().run_image(image=image, tool_alias=tool_alias, port=3001)
     
-    return 
+
+    response = requests.get(f"http://localhost:3001/tool/{tool_alias}/class-data/")
+    return response
 
 
 @app.post("/tool/{tool_alias}/run", status_code=200)
