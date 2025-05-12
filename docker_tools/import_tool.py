a1ed7de202229898f358b195ed92181e918860a7
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-05-12 11:23:24 +0000

    fix(chore): Deleted not used code (import tool).

--- docker_tools/import_tool.py
+++ docker_tools/import_tool.py
@@ -3,39 +3,10 @@ from docker.models.images import Image
 
 from build_langchain_tool import ToolDockerImageBuilder, get_image_by_name, client
 from base_models import *
-from docker_tools.proxy_tool_factory import ProxyToolFactory
+from proxy_tool_factory import ProxyToolFactory
 
-proxy_tool_factory = ProxyToolFactory()
+proxy_tool_factory = ProxyToolFactory(host="localhost", port=4800)
 
-def import_tools(import_tool_data: ImportToolData):
-    dependencies = import_tool_data.dependencies
-    force_build = import_tool_data.force_build
+def import_tool(tool_alias):
 
-    if dependencies is None:
-        dependencies = []
-
-    tdib = ToolDockerImageBuilder(
-        tool_dict=import_tool_data.tool_dict,
-        import_list=dependencies,
-    )
-
-    image: Image | None = get_image_by_name(
-        image_name=import_tool_data.image_name
-    )  # optimize
-
-    if force_build or not image:
-        image = tdib.build_tool(image_name=import_tool_data.image_name)[0]
-
-    # TODO: refactor
-
-    port = 5005
-
-    container_tool = client.containers.run(
-        image=image, ports={"8000/tcp": port}, detach=True
-    )
-
-    tool_alias_dict = dict()
-    for alias in import_tool_data.tool_dict.keys():
-        tool_alias_dict[alias] = proxy_tool_factory.create_proxy_class(alias)
-
-    return tool_alias_dict
+    return proxy_tool_factory.create_proxy_class(tool_alias)
