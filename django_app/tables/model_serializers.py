c8ab0c9222f8145cdf22157126ffbe92a34352cb
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-05-14 16:15:33 +0000

    feat: Added serializers for foreign keys

--- django_app/tables/model_serializers.py
+++ django_app/tables/model_serializers.py
@@ -15,12 +15,6 @@ from .models import (
 )
 
 
-class TemplateAgentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TemplateAgent
-        fields = "__all__"
-
-
 class ConfigLLMSerializer(serializers.ModelSerializer):
     class Meta:
         model = ConfigLLM
@@ -34,48 +28,85 @@ class ProviderSerializer(serializers.ModelSerializer):
 
 
 class LLMModelSerializer(serializers.ModelSerializer):
+    llm_provider = ProviderSerializer(read_only=True)
+
     class Meta:
         model = LLMModel
         fields = "__all__"
 
 
 class EmbeddingModelSerializer(serializers.ModelSerializer):
+    embedding_provider = ProviderSerializer(read_only=True)
+
     class Meta:
         model = EmbeddingModel
         fields = "__all__"
 
 
 class ManagerLLMModelSerializer(serializers.ModelSerializer):
+    llm_model = LLMModelSerializer(read_only=True)
+    llm_config = ConfigLLMSerializer(read_only=True)
+
     class Meta:
         model = ManagerLLMModel
         fields = "__all__"
 
 
 class ToolSerializer(serializers.ModelSerializer):
+    llm_model = LLMModelSerializer(read_only=True)
+    llm_config = ConfigLLMSerializer(read_only=True)
+
+    embedding_model = EmbeddingModelSerializer(read_only=True)
+
     class Meta:
         model = Tool
         fields = "__all__"
 
 
 class EnabledToolsSerializer(serializers.ModelSerializer):
+    tools = ToolSerializer(many=True, read_only=True)
+
     class Meta:
         model = EnabledTools
         fields = "__all__"
 
 
 class AgentSerializer(serializers.ModelSerializer):
+    tools = ToolSerializer(many=True, read_only=True)
+    llm_model = LLMModelSerializer(read_only=True)
+    llm_config = ConfigLLMSerializer(read_only=True)
+
+    fcm_llm_model = LLMModelSerializer(read_only=True)
+    fcm_llm_config = ConfigLLMSerializer(read_only=True)
+
     class Meta:
         model = Agent
         fields = "__all__"
 
 
+class TemplateAgentSerializer(serializers.ModelSerializer):
+    agent = AgentSerializer(read_only=True)
+
+    class Meta:
+        model = TemplateAgent
+        fields = "__all__"
+
+
 class CrewSerializer(serializers.ModelSerializer):
+    agents = AgentSerializer(many=True, read_only=True)
+    embedding_model = EmbeddingModelSerializer(read_only=True)
+    manager_llm_model = ManagerLLMModelSerializer(read_only=True)
+    manager_llm_config = ConfigLLMSerializer(read_only=True)
+
     class Meta:
         model = Crew
         fields = "__all__"
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    crew = CrewSerializer(read_only=True)
+    agent = AgentSerializer(read_only=True)
+
     class Meta:
         model = Task
         fields = "__all__"
