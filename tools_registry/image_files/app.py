7ede9f5899aa80caf0f26502106fba3f9871a7ab
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-05-15 01:22:21 +0000

    feat: Create parsers

--- tools_registry/image_files/app.py
+++ tools_registry/image_files/app.py
@@ -1,79 +1,57 @@
-import json
 import os
 import time
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
 import uvicorn
-from fastapi.responses import JSONResponse
-from models.models import (
-    RunToolParamsModel,
-    RunCrewModel,
-    ToolListResponseModel,
-    ClassDataResponseModel,
-    RunToolResponseModel,
-)
+import requests
+
+from models.models import RunToolParamsModel
 from repositories.import_tool_data_repository import ImportToolDataRepository
 from services.tool_image_service import ToolImageService
 from services.tool_container_service import (
     ToolContainerService,
 )
-
+from services.registry import Registry
 
 import docker
+from docker.models.containers import Container
 
 docker.from_env()
+client: Container = docker.client
 
 app = FastAPI()
+registry = Registry()
 import_tool_data_repository = ImportToolDataRepository()
 
 tool_image_service = ToolImageService(
+    registry=registry, 
     import_tool_data_repository=import_tool_data_repository
 )
 tool_container_service = ToolContainerService(
     tool_image_service=tool_image_service,
-    import_tool_data_repository=import_tool_data_repository,
+    import_tool_data_repository=import_tool_data_repository
 )
-# TODO ADD LOGGER
-# TODO add error handlers (if error in important request -send to some service)
 
-@app.post(
-    "/crew/run", status_code=200
-)
-def run(tool_alias: str, run_crew_model: RunCrewModel):
+
+@app.get("/tool/list", status_code=200)
+async def get_all_tool_aliases(tool_alias: str):
     return
 
 
-@app.get("/tool/list", status_code=200, response_model=ToolListResponseModel)
-def get_all_tool_aliases():
+@app.get("/tool/{tool_alias}/class-data", status_code=200)
+async def get_class_data(tool_alias: str):
 
-    return ToolListResponseModel(
-        tool_list=import_tool_data_repository.get_tool_alias_list()
+    return tool_container_service.request_class_data(tool_alias=tool_alias)
+
+
+@app.post("/tool/{tool_alias}/run", status_code=200)
+async def run(tool_alias: str, run_tool_params_model: RunToolParamsModel):
+
+    return tool_container_service.request_run_tool(
+        tool_alias=tool_alias, run_tool_params_model=run_tool_params_model
     )
 
 
-@app.get(
-    "/tool/{tool_alias}/class-data",
-    status_code=200,
-    response_model=ClassDataResponseModel,
-)
-def get_class_data(tool_alias: str):
-    classdata = tool_container_service.request_class_data(tool_alias=tool_alias)[
-        "classdata"
-    ]
-    return ClassDataResponseModel(classdata=classdata)
-
-
-@app.post(
-    "/tool/{tool_alias}/run", status_code=200, response_model=RunToolResponseModel
-)
-def run(tool_alias: str, run_tool_params_model: RunToolParamsModel):
-    # logger.debug(f"run tool {tool_alias} with params {run_tool_params_model.dict()}")
-    run_tool_data = tool_container_service.request_run_tool(
-        tool_alias=tool_alias, run_tool_params_model=run_tool_params_model
-    )["data"]
-    return RunToolResponseModel(data=run_tool_data)
-
-
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8001, reload=True, workers=1)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True, workers=1)
--- tools_registry/image_files/models/models.py
+++ tools_registry/image_files/models/models.py
@@ -1,24 +1,6 @@
 from typing import Any
 from pydantic import BaseModel
 
-
 class RunToolParamsModel(BaseModel):
     run_args: list[str]
     run_kwargs: dict[str, Any]
-
-
-class RunCrewModel(BaseModel):
-    data: dict[str, Any]
-
-
-class ToolListResponseModel(BaseModel):
-    tool_list: list[str]
-
-class ClassDataResponseModel(BaseModel):
-    classdata: str
-
-
-class RunToolResponseModel(BaseModel):
-    data: str
-
-
