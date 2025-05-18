aa39f156f3be8be66c26dffc3573dbd5191ea39d
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-05-18 03:54:08 +0000

    feat(tests/main_crew_parser): Added load env and parse json to pydantic

--- tests/main_crew_parser/parser_test.py
+++ tests/main_crew_parser/parser_test.py
@@ -1,15 +1,17 @@
 import json
 from pathlib import Path
 from src.crew.fastapi.services.crew_parser import CrewParser
-
-
+from src.crew.fastapi.models.request_models import CrewData
+from src.crew.utils.helpers import load_env
 with open(Path("tests/main_crew_parser/test_json.json"), "r") as f:
     test_json = f.read()
 
-crew_data = json.loads(test_json)
+parser = CrewParser(tool_registry_host="localhost", tool_registry_port=8001)
 
-parser = CrewParser(tool_registry_host="localhost", tool_registry_port=4800)
 
+crew_data = CrewData.model_validate_json(test_json)
+
+load_env(Path("config/config.yaml").resolve().as_posix())
 crew = parser.parse_crew(crew_data=crew_data)
 
 print(crew.id)
