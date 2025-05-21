6d5250873245cf67f35cd93d73f9e29096ffaeed
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-05-21 09:18:01 +0000

    feat(crew/fastapi_app): added container manager service to endpoint, changed crew result

--- src/crew/fastapi_app/app.py
+++ src/crew/fastapi_app/app.py
@@ -1,34 +1,32 @@
-import os
-import time
-
-from crewai import Crew
-from dotenv import load_dotenv
 from fastapi import FastAPI, Response, status
 import uvicorn
-from fastapi_app.models.request_models import RunCrewModel
-from fastapi_app.services.process_service import ProcessService
-import multiprocessing as mp
+
+from services.container_manager_service import ContainerManagerService
+from services.process_service import ProcessService
+from models.request_models import RunCrewModel
 
 process_service = ProcessService()
+container_manager_service = ContainerManagerService()
+
+
 app = FastAPI()
 
 
 @app.post("/crew/run", status_code=200)
 def run_crew(run_crew_model: RunCrewModel):
+    container_manager_service.set_env("CREW_ID", str(run_crew_model.data.id))
 
-    
     process_service.run_process(run_crew_model.data.model_dump_json())
-    
+
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 @app.get("/result/crew/{crew_id}", status_code=200)
 def get_result(crew_id: int):
 
-    
-    result = process_service.get_result_by_id(crew_id=crew_id)
-    
-    return Response(content=result, status_code=status.HTTP_200_OK)
+    result = process_service.get_crew_output_json(crew_id=crew_id)
 
+    return Response(content=result, status_code=status.HTTP_200_OK)
 
 
 if __name__ == "__main__":
