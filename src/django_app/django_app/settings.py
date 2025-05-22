fad12b6768228a6d203b55fc352208afd50ff319
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-22 11:22:29 +0000

    chore: add django-filters to installed apps

--- src/django_app/django_app/settings.py
+++ src/django_app/django_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "tables",
     "rest_framework",
     "drf_yasg",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,7 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 50,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 
