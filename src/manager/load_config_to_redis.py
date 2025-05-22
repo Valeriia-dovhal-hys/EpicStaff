c9c4384ff4610ac715e83746201c6e3ba6b382ac
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-22 18:53:21 +0000

    feat: add func that loads tool_config to redis when container runs

---
+++ src/manager/load_config_to_redis.py
@@ -0,0 +1,15 @@
+import json
+import redis
+import os
+
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = int(os.getenv("REDIS_PORT", 6379))
+
+redis_client = redis.Redis(host=redis_host, port=redis_port)
+
+with open('tools_config.json', 'r') as f:
+    config_data = json.load(f)
+
+for item in config_data:
+    tool_key = list(item["tool_dict"].keys())[0]
+    redis_client.set(tool_key, json.dumps(item))
