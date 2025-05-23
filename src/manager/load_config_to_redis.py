d42d6d135679a2708bf29913dbd338b1d51a2ab1
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-23 12:35:12 +0000

    fix: write tool aliases to hash map instead

--- src/manager/load_config_to_redis.py
+++ src/manager/load_config_to_redis.py
@@ -10,6 +10,9 @@ redis_client = redis.Redis(host=redis_host, port=redis_port)
 with open('tools_config.json', 'r') as f:
     config_data = json.load(f)
 
+tools = {}
 for item in config_data:
-    tool_key = list(item["tool_dict"].keys())[0]
-    redis_client.set(tool_key, json.dumps(item))
+    tool_alias = list(item["tool_dict"].keys())[0]
+    redis_client.hset('tools', tool_alias, json.dumps(item))
+
+    
