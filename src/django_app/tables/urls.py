26229c2381bd1600ecd62ae2120d0dde79a938fc
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-05-24 12:32:27 +0000

    Merge remote-tracking branch 'origin/Dev' into 50-cors-headers

--- src/django_app/tables/urls.py
+++ src/django_app/tables/urls.py
@@ -21,6 +21,7 @@ from .views.views import (
     RunCrew,
     GetUpdates,
     StopSession,
+    getToolAliases,
 )
 
 router = DefaultRouter()
@@ -42,6 +43,7 @@ router.register(r"sessions", SessionViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("run-crew/", RunCrew.as_view(), name="run-crew"),
+    path("tool-aliases/", getToolAliases, name="tool-aliases"),
     path("answer-to-llm/", AnswerToLLM.as_view(), name="answer-to-llm"),
     path(
         "sessions/<int:session_id>/get-updates/",
