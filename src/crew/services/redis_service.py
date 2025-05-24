57cbaac2e796a2fb2272f400aad6c17fbaf431f5
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-05-24 01:22:43 +0000

    chore(crew/services): removed unsed imports and refactor service

--- src/crew/services/redis_service.py
+++ src/crew/services/redis_service.py
@@ -1,7 +1,7 @@
 import json
 import os
-from threading import Thread
-from typing import Callable, Protocol
+from typing import  Protocol
+from services.container_manager_service import ContainerManagerService
 
 from redis import Redis
 
@@ -12,16 +12,28 @@ class MessageHandler(Protocol):
 
 class RedisService:
 
-    def __init__(self, crew_id: int, redis_host=None):
+    def __init__(
+        self,
+        container_manager_service: ContainerManagerService,
+        crew_id: int | None = None,
+        redis_host: str | None = None,
+    ):
         if redis_host is None:
             redis_host = os.environ.get("PROCESS_REDIS_HOST", "redis")
         self.redis_host = redis_host
-        self.redis_client = Redis(redis_host=redis_host, decode_responses=True)
-        self.crew_id = crew_id
+        self.redis_client = Redis(host=redis_host, decode_responses=True)
+        self.container_manager_service = container_manager_service
+        self.crew_id = (
+            crew_id if crew_id is not None else container_manager_service.get_crew_id()
+        )
 
     def publish(self, channel: str, message: str):
         channel_name = f"{self.crew_id}:{channel}"
         self.redis_client.publish(channel=channel_name, message=json.dumps(message))
 
-    def get_json_crew_schema(self, crew_id: int) -> str | None:
-        return self.redis_client.get(f"{crew_id}:schema")
+    def read(self, channel: str):
+        channel_name = f"crews:{self.crew_id}:{channel}"
+        return self.redis_client.get(channel_name)
+
+    def get_json_crew_schema(self) -> str | None:
+        return self.read("schema")
--- src/crew/services/run_crew_service.py
+++ src/crew/services/run_crew_service.py
@@ -1,34 +1,24 @@
-import json
-import os
 from pathlib import Path
 
 from crewai import Crew
-from services.container_manager_service import ContainerManagerService
 from services.redis_service import RedisService
 from models.request_models import CrewData
 from services.crew_parser import CrewParser
 from utils.helpers import load_env
-import multiprocessing as mp
-from redis import Redis
+
 
 
 class RunCrewService:
     def __init__(
         self,
         redis_service: RedisService,
-        container_manager_service: ContainerManagerService,
     ):
-
         self.redis_service = redis_service
-        self.container_manager_service = container_manager_service
         self.crew_parser = CrewParser()
 
-
-
     def run(self):
-        crew_id = self.container_manager_service.get_crew_id()
 
-        json_crew_schema = self.redis_service.get_json_crew_schema(crew_id=crew_id)
+        json_crew_schema = self.redis_service.get_json_crew_schema()
 
         crew_data = CrewData.model_validate_json(json_crew_schema)
 
