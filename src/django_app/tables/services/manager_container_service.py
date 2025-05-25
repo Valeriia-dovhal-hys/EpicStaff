1e8ea4b1ef655913bbb5fcbaf9ea77334e534a65
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-25 16:16:30 +0000

    Merge branch '52-tool_aliases-in-db' into 'Dev'
    Resolve "tool_aliases in db"
    
    See merge request sheetsui/crewai-sheetsui!47

---
+++ src/django_app/tables/services/manager_container_service.py
@@ -0,0 +1,16 @@
+import requests
+from tables.serializers.nested_model_serializers import NestedCrewSerializer
+
+class ManagerContainerService:
+    def __init__(self, base_url: str):
+        self.base_url = base_url
+
+    def run_crew(self, serialized_crew) -> dict:
+        url = f"{self.base_url}/crew/run"
+        payload = {"data": serialized_crew}
+        response = requests.post(url, json=payload)
+        
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
--- src/django_app/tables/services/session_manager_service.py
+++ src/django_app/tables/services/session_manager_service.py
@@ -1,29 +1,16 @@
-from tables.services.message_service import MessageService
-from tables.services.crew_service import CrewService
-from tables.services.redis_service import RedisService
 from tables.models import Session
 
 
 class SessionManagerService:
 
-    def __init__(
-        self,
-        redis_service: RedisService,
-        crew_service: CrewService,
-        message_service: MessageService,
-    ) -> None:
-        self.message_service = message_service
-        self.redis_service = redis_service
-        self.crew_service = crew_service
+    def __init__(self) -> None:
+        pass
 
     def get_session(self, session_id: int) -> Session:
         return Session.objects.get(id=session_id)
 
     def stop_session(self, session_id: int) -> None:
         session: Session = self.get_session(session_id=session_id)
-        self.message_service.unsubscribe_for_messages(crew_id=session.crew.pk)
-        # TODO: Send notify to redis channel to stop container
-
         session.status = Session.SessionStatus.END
         session.save()
 
@@ -31,20 +18,5 @@ class SessionManagerService:
         session: Session = self.get_session(session_id=session_id)
         return session.status
 
-    def create_session(self, crew_id: int) -> int:
-        session = Session.objects.create(crew_id=crew_id, status=Session.SessionStatus.RUN)
-        return session.pk
-
-
-    def session_run_crew(self, session_id: int) -> None:
-        session: Session = self.get_session(session_id=session_id)
-        crew_json_schema = self.crew_service.create_crew_schema_json(
-            crew_id=session.crew.pk
-        )
-        # CheckStatus
-        self.redis_service.set_crew_data(
-            crew_id=session.crew.pk, crew_json_schema=crew_json_schema
-        )
-        self.redis_service.publish_start_crew(session=session)
-
-        self.message_service.subscribe_for_messages(session=session)
+    def create_session(self, crew_id: int) -> Session:
+        return Session.objects.create(crew_id=crew_id, status=Session.SessionStatus.RUN)
