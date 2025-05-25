7e1c9bb1c158024b5317acbb95004e689f5f93c5
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-25 03:52:02 +0000

    fix(crew): fix bug with nested TaskData in CrewData

--- src/crew/models/request_models.py
+++ src/crew/models/request_models.py
@@ -77,7 +77,7 @@ class CrewData(BaseModel):
     embedding_model: Optional[EmbeddingModelData] = None
     manager_llm_model: Optional[LLMModelData] = None
     manager_llm_config: Optional[ConfigLLMData] = None
-    tasks: List["TaskData"] | None
+    tasks: List["TaskData"] | None = None
 
 
 class TaskData(BaseModel):
