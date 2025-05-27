8b117a10db6fd32e43a41cf4113c6568cca9e6aa
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-27 07:34:12 +0000

    Merge branch 'Dev' into 54-change-crew-schema-to-session

--- src/django_app/tables/services/session_manager_service.py
+++ src/django_app/tables/services/session_manager_service.py
@@ -1,16 +1,25 @@
+from tables.services.crew_service import CrewService
+from tables.services.redis_service import RedisService
 from tables.models import Session
 
 
 class SessionManagerService:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        redis_service: RedisService,
+        crew_service: CrewService,
+    ) -> None:
+        self.redis_service = redis_service
+        self.crew_service = crew_service
 
     def get_session(self, session_id: int) -> Session:
         return Session.objects.get(id=session_id)
 
     def stop_session(self, session_id: int) -> None:
         session: Session = self.get_session(session_id=session_id)
+        # TODO: Send notify to redis channel to stop container
+
         session.status = Session.SessionStatus.END
         session.save()
 
@@ -18,5 +27,20 @@ class SessionManagerService:
         session: Session = self.get_session(session_id=session_id)
         return session.status
 
-    def create_session(self, crew_id: int) -> Session:
-        return Session.objects.create(crew_id=crew_id, status=Session.SessionStatus.RUN)
+    def create_session(self, crew_id: int) -> int:
+        session = Session.objects.create(crew_id=crew_id, status=Session.SessionStatus.RUN)
+        return session.pk
+
+
+    def session_run_crew(self, session_id: int) -> None:
+        session: Session = self.get_session(session_id=session_id)
+        crew_json_schema = self.crew_service.create_crew_schema_json(
+            crew_id=session.crew.pk
+        )
+        # CheckStatus
+        self.redis_service.set_crew_data(
+            crew_id=session.crew.pk, crew_json_schema=crew_json_schema
+        )
+        self.redis_service.publish_start_crew(crew_id=session.crew.pk)
+
+        
