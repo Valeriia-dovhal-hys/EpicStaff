3b50c0d22ce10df668724aeb27fe4a5743fc4576
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-30 07:31:03 +0000

    chore(crew): add SessionStatus enum

--- src/crew/models/response_models.py
+++ src/crew/models/response_models.py
@@ -1,5 +1,13 @@
+from enum import Enum
 from pydantic import BaseModel
 
 
 class RunCrewModel(BaseModel):
     crew_id: int
+
+
+class SessionStatus(Enum):
+        END = "end"
+        RUN = "run"
+        WAIT_FOR_USER = "wait_for_user"
+        ERROR = "error"
