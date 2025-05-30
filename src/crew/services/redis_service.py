0fca53ca6bf2162f4758020047d49b2c3dc86b0b
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-05-30 08:12:57 +0000

    feat(crew): add publish_session_status & refactor

--- src/crew/services/redis_service.py
+++ src/crew/services/redis_service.py
@@ -2,6 +2,7 @@ import json
 import os
 from typing import Protocol
 from services.container_manager_service import ContainerManagerService
+from models.response_models import SessionStatus
 
 from redis import Redis
 
@@ -27,10 +28,24 @@ class RedisService:
             session_id if session_id is not None else container_manager_service.get_session_id()
         )
 
-    def publish(self, channel: str, message: str):
+
+    def _publish(self, channel: str, message):
         channel_name = f"sessions:{channel}"
         self.redis_client.publish(channel=channel_name, message=json.dumps(message))
+
 
     def get_json_session_schema(self) -> str | None:
         channel_name = f"sessions:{self.session_id}:schema"
         return self.redis_client.get(channel_name)
+
+
+    def publish_session_status(self, session_status: SessionStatus):
+        message = {
+            'session_id': self.session_id,
+            'status': session_status.value,
+        }
+        self._publish("session_status", message)
+
+    
+    def publish_final_result(self, final_result: str):
+        self._publish("final_result", final_result)
