541f4e305a2ffedfbb55dfeab842b10f77a019ae
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-05-31 06:20:03 +0000

    fix(manager): fixed finding crew image by name

--- src/manager/services/crew_image_service.py
+++ src/manager/services/crew_image_service.py
@@ -1,32 +1,32 @@
 from pathlib import Path
+from venv import logger
 
 import docker
 from docker.models.images import Image
+
 
 class CrewImageService:
 
     def __init__(self):
         self.client = docker.client.from_env()
 
-
     def build_image(self) -> Image:
 
-        dockerfile = Path('./crew/Dockerfile.crew').resolve().as_posix()
-        image_files = Path('./crew').resolve().as_posix()
+        dockerfile = Path("./crew/Dockerfile.crew").resolve().as_posix()
+        image_files = Path("./crew").resolve().as_posix()
 
         image = self.client.images.build(
             path=image_files,
-            tag='crew',
+            tag="crew",
             dockerfile=dockerfile,
         )[0]
 
         return image
 
-    
     def get_image(self):
-        
-        image_list = self.client.images.list(filters={"label": "crew"})
+
+        image_list = self.client.images.list(name="crew")
         if image_list:
             return image_list[0]
-        
+
         return self.build_image()
