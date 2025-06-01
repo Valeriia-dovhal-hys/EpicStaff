9346e4e3516b8150366c2acae7aa942b3fb76423
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-06-01 12:31:30 +0000

    chore(manager/tests): move container_builder

--- src/manager/tests/container_builder/container_builder.py
+++ src/manager/tests/container_builder/container_builder.py
@@ -4,14 +4,15 @@ from docker.models.containers import Container
 from docker.errors import ImageNotFound, NotFound
 from pathlib import Path
 
+
 # TODO IF NEEDED
 class ManagerContainerBuilder:
 
     client = docker.client.from_env()
     image_name = "manager"
     port = 8001
-    dockerfile = Path('./manager/image_files/Dockerfile.reg').resolve().as_posix()
-    path = Path('./manager/image_files').resolve().as_posix()
+    dockerfile = Path("./manager/Dockerfile.reg").as_posix()
+    path = Path("./src").as_posix()
     container_name = "manager_container"
 
     def __init__(self, *, image_name="manager", port=8001, force_build=False):
@@ -19,17 +20,16 @@ class ManagerContainerBuilder:
         self.port = port
         self.force_build = force_build
 
-
     def run_container(self) -> Container:
         """
-        Run a Docker container from the specified image. If the container 
+        Run a Docker container from the specified image. If the container
         already exists and is running, it will return the existing container.
         Otherwise, it will start a new one.
         """
 
         try:
             container = self.client.containers.get(self.container_name)
-            if container.status == 'running':
+            if container.status == "running":
                 return container
             else:
                 container.start()
@@ -37,39 +37,32 @@ class ManagerContainerBuilder:
         except NotFound:
             # TODO: Log here that container does not exist and it's going to build it
             pass
-    
+
         image = self.build_image()
         container = self.client.containers.run(
             image=self.image_name,
             ports={"8000/tcp": self.port},
             volumes={
-                '/var/run/docker.sock': { 
-                    'bind': '/var/run/docker.sock', 
-                    'mode': 'rw'
-                },
-
-                '/usr/bin/docker': {
-                    'bind': '/usr/bin/docker', 
-                    'mode': 'rw'
-                }
+                "/var/run/docker.sock": {"bind": "/var/run/docker.sock", "mode": "rw"},
+                "/usr/bin/docker": {"bind": "/usr/bin/docker", "mode": "rw"},
             },
             tty=True,
             stdin_open=True,
             detach=True,
-            network='my-net',
+            network="my-net",
             name=self.container_name,
         )
         return container
 
-
     def build_image(self) -> Image:
         """
-        Build the Docker image if it does not exist. If the image already 
+        Build the Docker image if it does not exist. If the image already
         exists, it returns the existing image.
         """
 
         try:
-            if self.force_build: raise ImageNotFound("workaround")
+            if self.force_build:
+                raise ImageNotFound("workaround")
             image = self.client.images.get(self.image_name)
         except ImageNotFound:
             image, _ = self.client.images.build(
@@ -79,9 +72,8 @@ class ManagerContainerBuilder:
             )
 
         return image
-    
+
 
 if __name__ == "__main__":
     builder = ManagerContainerBuilder(force_build=True)
     container = builder.run_container()
-
