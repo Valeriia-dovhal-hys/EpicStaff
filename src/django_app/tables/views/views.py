d9b8556d90393b0a101f76923e3bc74ba60d1539
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-06-02 05:07:17 +0000

    chore(django_app):  init services in django_app/tables/apps.py

--- src/django_app/tables/views/views.py
+++ src/django_app/tables/views/views.py
@@ -7,7 +7,6 @@ from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from django.core.paginator import Paginator, EmptyPage
 
 from tables.services.config_service import YamlConfigService
 from tables.services.session_manager_service import SessionManagerService
@@ -33,10 +32,7 @@ from tables.serializers.nested_model_serializers import (
 redis_service = RedisService()
 crew_service = CrewService()
 session_runner_service = SessionRunnerService()
-session_manager_service = SessionManagerService(
-    redis_service=redis_service,
-    crew_service=crew_service,
-)
+session_manager_service = SessionManagerService()
 config_service = YamlConfigService()
 
 
