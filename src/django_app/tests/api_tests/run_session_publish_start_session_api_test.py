b604086137243cf3a26f8000d7a889981cb7bf3b
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-06-02 09:19:23 +0000

    feat(django_app/test): Added api run session tests

--- src/django_app/tests/api_tests/run_session_publish_start_session_api_test.py
+++ src/django_app/tests/api_tests/run_session_publish_start_session_api_test.py
@@ -1,18 +1,16 @@
 import pytest
 from django.urls import reverse
-from tables.models import Session
 from tests.fixtures import *
 
 
 @pytest.mark.django_db
-def test_run_session(crew, api_client, redis_client_mock):
+def test_run_session_publish_start_session(crew, api_client, redis_client_mock):
 
-    url = reverse("run-session")
     data = {"crew_id": crew.pk}
+    url = reverse("run-session")
 
     response = api_client.post(url, data, format="json")
 
     response_session_id: int = response.data["session_id"]
-    session = Session.objects.get(pk=response_session_id)
 
-    assert session.crew.pk == crew.pk
+    redis_client_mock.publish.assert_called_once_with("sessions:start", response_session_id)
