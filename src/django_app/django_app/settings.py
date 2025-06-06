c62968bd2868ef2b3b0027cbd412598dc53d87a7
Ihor Orlenko <ihor.orlenko@hys-enterprise.com>
2025-06-06 06:16:19 +0000

    chore(BE | django_app): add exception logic for when crew creation is insuccesfull

--- src/django_app/django_app/settings.py
+++ src/django_app/django_app/settings.py
@@ -63,7 +63,10 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
 
 ROOT_URLCONF = "django_app.urls"
 
