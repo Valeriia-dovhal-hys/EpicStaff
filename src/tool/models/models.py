53fbe7c234dd744e2585e78fe4ee980464413bef
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-06-07 08:11:45 +0000

    fix(BE | tool): added tool_config to RunToolParamsModel

--- src/tool/models/models.py
+++ src/tool/models/models.py
@@ -3,6 +3,7 @@ import typing
 from pydantic import BaseModel
 
 class RunToolParamsModel(BaseModel):
+    tool_config: dict[str, Any] | None = None
     run_args: list[str]
     run_kwargs: dict[str, Any]
 
