8d3e9d9cddb771a6823dd5ad20704537e48f1fd0
Maksym Yevstratiev <maksym.yevstratiev@hys-enterprise.com>
2025-06-09 03:50:59 +0000

    test(BE | django_app): create environment api test structure

---
+++ src/django_app/tests/api_tests/environment_api_test.py
@@ -0,0 +1,20 @@
+import pytest
+from django.urls import reverse
+from tables.models import Session
+from tests.fixtures import *
+
+
+@pytest.mark.django_db
+def test_get_environment_config():
+
+    pass
+
+
+@pytest.mark.django_db
+def test_post_environment_config():
+    pass
+
+
+@pytest.mark.django_db
+def test_delete_environment_config():
+    pass
